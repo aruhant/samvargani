833fd1933adcb8084ac096725cc9c3bb3e5e2a84
Aruhant <125254014+aruhant@users.noreply.github.com>
2023-07-31 22:54:10 -0700

    added new words

--- app/lib/models/answer.dart
+++ app/lib/models/answer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
 class GameAnswer {
   final String answer;
@@ -15,7 +16,7 @@ class GameAnswer {
     this.hints,
     this.colors = const [Colors.white, Colors.blue],
     this.backgroundColor = const Color.fromARGB(255, 180, 207, 229),
-    this.icons = const [Icons.cloud],
+    this.icons = const [LineIcons.cloud, LineIcons.star],
     this.images,
     this.moveHorizontal = true,
     this.moveVertical = false,
@@ -23,6 +24,38 @@ class GameAnswer {
 }
 
 final List<GameAnswer> gameAnswers = [
+  GameAnswer(
+      answer: 'स्नेह',
+      meaning: 'प्रेम',
+      icons: [LineIcons.heart],
+      moveHorizontal: false,
+      moveVertical: true,
+      colors: [Colors.pink, Colors.red],
+      backgroundColor: Colors.pink[50]),
+  GameAnswer(
+      answer: 'वृष्टि',
+      meaning: 'बारिश',
+      icons: [LineIcons.cloud],
+      moveHorizontal: true,
+      moveVertical: false,
+      colors: [Colors.blue, Colors.cyan],
+      backgroundColor: Colors.blue[100]),
+  GameAnswer(
+      answer: 'वारिद',
+      meaning: 'बारिश',
+      icons: [LineIcons.cloud],
+      moveHorizontal: true,
+      moveVertical: false,
+      colors: [Colors.blue, Colors.cyan],
+      backgroundColor: Colors.blue[100]),
+  GameAnswer(
+      answer: 'वारिद',
+      meaning: 'बारिश',
+      icons: [LineIcons.cloud],
+      moveHorizontal: true,
+      moveVertical: false,
+      colors: [Colors.blue, Colors.cyan],
+      backgroundColor: Colors.blue[100]),
   GameAnswer(answer: 'श्रृंगार', meaning: 'सजाने की क्रिया या भाव'),
   GameAnswer(answer: 'उत्सुकता', meaning: 'उत्साह'),
   GameAnswer(answer: 'साहसिक', meaning: 'बहादुर'),
@@ -30,7 +63,14 @@ final List<GameAnswer> gameAnswers = [
   GameAnswer(answer: 'उमंग', meaning: 'उत्साह'),
   GameAnswer(answer: 'प्रचंड', meaning: 'बहुत अधिक'),
   GameAnswer(answer: 'मुस्कान', meaning: 'हँसी'),
-  GameAnswer(answer: 'स्नेह', meaning: 'प्रेम'),
+  GameAnswer(
+      answer: 'स्नेह',
+      meaning: 'प्रेम',
+      icons: [LineIcons.heart],
+      moveHorizontal: false,
+      moveVertical: true,
+      colors: [Colors.pink, Colors.red],
+      backgroundColor: Colors.pink[50]),
   GameAnswer(answer: 'बेख़बर', meaning: 'अनजान'),
   GameAnswer(answer: 'धैर्य', meaning: 'साहस'),
 ];
