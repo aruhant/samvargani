12b8c7377cde1f7cdf060525aa7b1d1dd4952d1d
Aruhant <125254014+aruhant@users.noreply.github.com>
2023-08-07 21:42:02 -0700

    +share

--- app/lib/utils/share.dart
+++ app/lib/utils/share.dart
@@ -1,1 +1,18 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:screenshot/screenshot.dart';
+
+shareScreenShot(ScreenshotController screenshotController) {
+  screenshotController
+      .capture(delay: const Duration(milliseconds: 10))
+      .then((image) async {
+    if (image != null) {
+      final directory = await getApplicationDocumentsDirectory();
+      final imagePath = await File('${directory.path}/image.png').create();
+      await imagePath.writeAsBytes(image);
+      await Share.shareXFiles([XFile(imagePath.path)],
+          text: 'Share With Friends!');
+    }
+  });
+}
