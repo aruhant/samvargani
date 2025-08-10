0f9ece3f719584db4b54a5089aebf28df2948f3c
aruhant <125254014+aruhant@users.noreply.github.com>
2025-08-10 15:30:40 -0700

    responding to SDK updates

--- app/android/app/src/main/kotlin/com/aruhant/paheli/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/aruhant/paheli/MainActivity.kt
@@ -2,4 +2,4 @@ package com.aruhant.paheli
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,178 +1,30 @@
-import 'package:paheli/utils/dictionary.dart';
-import 'package:paheli/utils/string.dart';
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:paheli/main.dart';
 
 void main() {
-  for (final word in wordList) {
-    if (word.allCharacters.length == 2 &&
-        word.allCharacters[0].matra.isEmpty &&
-        word.allCharacters[1].matra.isEmpty &&
-        // ends with r
-        word.endsWith('र')) {
-      print(word);
-    }
-  }
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-  // print('ढ़'.contains('़'));
-  // print('टढ़ड़ा'.getAllVaraitions);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-  /*
-  for (String character in 'क़'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly}+ ${character.matra}');
-  }
-  // for
-  print('------------------');
-  for (String character in 'क़'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly}+ ${character.matra}');
-  }
-  print('------------------');
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
-  for (String character in 'सामर्थ्य'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly}+ ${character.matra}');
-  }
-  print('------------------');
-  // do the same for अर्थशास्त्र
-  for (String character in 'अर्थशास्त्र'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly}+ ${character.matra}');
-  }
-  print('------------------');
-  for (String character in 'उत्सुक'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra}');
-  }
-
-  print('------------------');
-  for (String character in 'त्रिशूल'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra}');
-  }
-  // do the same for स्री
-  print('------------------');
-  for (String character in 'स्त्री'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra}');
-  }
-  print('------------------');
-  // परिप्रेक्ष्य
-
-  for (String character in 'परिप्रेक्ष्क'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra}');
-  }
-  // do the same for श्रेष्ठतम, सर्वश्रेष्ठ, उत्कृष्ट
-  print('------------------');
-  for (String character in 'श्रेष्ठतम'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra}');
-  }
-  print('------------------');
-  for (String character in 'सर्वश्रेष्ठ'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra}');
-  }
-  print('------------------');
-  for (String character in 'उत्कृष्ट'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra}');
-  }
-  // do the same for शृंगार
-
-  print('------------------');
-  for (String character in 'शृंगार'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra} ');
-  }
-  // श्रृंगार
-  print('------------------');
-  for (String character in 'श्रृंगार'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra} ');
-  }
-  //चिह्न
-  print('------------------');
-  for (String character in 'चिह्न'.allCharacters) {
-    //print(character.runes.map((e) => String.fromCharCode(e)).toList());
-
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra} ');
-  }
-  // मुद्रित
-  print('------------------');
-  for (String character in 'मुद्रित'.allCharacters) {
-    //print(character.runes.map((e) => String.fromCharCode(e)).toList());
-
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra} ');
-  }
-  // पञ्चमाक्षर
-  print('------------------');
-  for (String character in 'पञ्चमाक्षर'.allCharacters) {
-    print(
-        // '${character.runes.map((e) => Char(e))} ---> ${character.matra} + ${character.vyanjan}');
-
-        '$character ---> ${character.matraOnly} + ${character.vyanjan} + ${character.halfOnly} + ${character.matra} ');
-  }
-  */
-  // test toJson of answer
-  /*final anAnswer = GameAnswer(
-        answer: 'a',
-        meaning: 'a',
-        images: [],
-        backgroundColor: const Color.fromARGB(2, 2, 2, 2),
-        colors: [Colors.black12],
-        icons: [LineIcons.handshake]);
-    final json = anAnswer.toJson();
-
-    print(json);
-
-    print('----------------------');
-    final fromJson = GameAnswer.fromJson(json);
-    print('----------------------');
-    print(fromJson.toJson());*/
-/*
-    Line line = Line(cells: [
-      Cell('a', state: CellState.misplaced),
-      Cell('b', state: CellState.misplaced),
-      Cell('c', state: CellState.misplacedVyanjan),
-      Cell('d', state: CellState.correctVyanjan),
-    ]);
-    Game game = Game(
-        onSuceess: (p0) {},
-        answer: GameAnswer(
-            answer: 'a',
-            meaning: 'a',
-            images: [],
-            backgroundColor: const Color.fromARGB(2, 2, 2, 2),
-            colors: [Colors.black12],
-            icons: [LineIcons.handshake]),
-        loadLines: [line]);
-    Map s = line.toJson();
-    print(s);
-    Line l = Line.fromJson(jsonDecode(jsonEncode(s)));
-    print(l.toJson());
-    print('----------------------');
-    print(game.toJson());
-    print('----------------------');
-    print(Game.fromJson(jsonDecode(jsonEncode(game.toJson()))).toJson());*/
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
 }
