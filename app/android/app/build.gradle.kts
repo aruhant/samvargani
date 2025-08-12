5edd057eb5dad2f0291e319188d34decba7932de
aruhant <125254014+aruhant@users.noreply.github.com>
2025-08-11 17:51:45 -0700

    responding to SDK updates

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -35,11 +35,20 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("$rootDir/app/enthrall.jks")
+            storePassword = "enthrall"
+            keyAlias = "enthrall"
+            keyPassword = "enthrall"
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
