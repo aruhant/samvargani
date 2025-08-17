6add0ded1c1b7a5880088c5afbe6614ee91f2150
aruhant <125254014+aruhant@users.noreply.github.com>
2025-08-16 18:03:14 -0700

    updated plugins

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -27,9 +27,7 @@ android {
         applicationId = "com.aruhant.paheli"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        //minSdk = flutter.minSdkVersion
-        minSdk = 23
-
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
